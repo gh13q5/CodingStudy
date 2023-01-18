74c555e00727ff7cd565f01b059c2003ca725f8d
g.h <35606917+gh13q5@users.noreply.github.com>
2023-01-18 18:45:59 +0900

    Create 완주하지 못한 선수_최가희.cpp

---
+++ 1주차(해시)/완주하지 못한 선수_최가희.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+string solution(vector<string> participant, vector<string> completion) {
+    string answer = "";
+    
+    sort(participant.begin(), participant.end());
+    sort(completion.begin(), completion.end());
+    
+    for(int i = 0; i < completion.size(); i++){
+        if(participant[i] != completion[i]){
+            answer = participant[i];
+            break;
+        }
+        if(i == completion.size() - 1){
+            answer = participant[i + 1];
+        }
+    }
+    
+    return answer;
+}
